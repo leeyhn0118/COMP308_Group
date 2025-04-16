fd3cdfd1f10cc7fb641fc64244c08cb201345489
Jay <hsong45@my.centennialcollege.ca>
2025-04-15 21:11:22 -0400

    error

--- server/microservices/auth-service/config/config.js
+++ server/microservices/auth-service/config/config.js
@@ -1,8 +1,9 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  db: process.env.AUTH_MONGO_URI || 'mongodb://localhost:27017/authServiceDB',
-  JWT_SECRET: process.env.JWT_SECRET || 'fallback_secret',
+  db: process.env.AUTH_MONGO_URI || "mongodb://localhost:27017/authServiceDB",
+  JWT_SECRET: process.env.JWT_SECRET || "fallback_secret",
   port: process.env.AUTH_PORT || 4001,
 };
+
